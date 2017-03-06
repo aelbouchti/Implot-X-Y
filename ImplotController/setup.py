13a0fdb3677d0eb47f7915449f659a2bc69a8aa2
Hilaly <hilalyamine@gmail.com>
2017-03-06 23:54:52 +0100

    Update setup.py

--- ImplotController/setup.py
+++ ImplotController/setup.py
@@ -14,8 +14,8 @@ setup(
     version='0.1.0',
     description='Sample package for Python-Guide.org',
     long_description=readme,
-    author='Kenneth Reitz',
-    author_email='me@kennethreitz.com',
+    author='Mohammed-Amine Hilaly',
+    author_email='Impcreativity@gmail.com',
     url='https://github.com/kennethreitz/samplemod',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
