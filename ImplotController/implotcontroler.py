700d16432ab117c06d07c593a451a22dabaad2be
Hilaly <hilalyamine@gmail.com>
2017-03-06 22:28:22 +0100

    Add files via upload

---
+++ ImplotController/implotcontroler.py
@@ -0,0 +1,70 @@
+
+
+
+# Imperial Creativity
+# ImplotControler 1.0
+# M-A Hilaly
+
+
+# ImplotControler is the 2D-implot main driver
+
+#
+
+from math import *
+from time import *
+from GPIO import *
+
+
+# Constants
+
+
+MotorX,MotorY
+StepX,StepY
+Xmax,Ymax
+NUL=0
+ERROR
+Grid
+Data
+
+# Classes
+
+class Motor:
+
+class Point:
+
+class Path:
+    
+
+
+# Functions
+
+def _init_():
+    
+def MoveX(Numberofsteps,sens):
+
+def MoveY(Numberofsteps,sens):
+
+def MoveStepX(sens):
+
+def MoveStepY(sens):
+
+def DrawPath():
+
+def Bresenham(Form,Precision):
+    
+def MTP(Point):
+    #Move to point
+
+
+def GoHome():
+    #go to initial point
+
+def printAlpha(alphabet):
+    #Print alphabet
+
+def printText(text):
+
+def estimtime():
+    
+def main():
+    
