39c9cfb5812d88d84cf289973ad5980cfaaa02d1
Hilaly <hilalyamine@gmail.com>
2017-03-07 16:50:14 +0100

    Update Point.py

--- ImplotController/Impcontroler/Point.py
+++ ImplotController/Impcontroler/Point.py
@@ -10,28 +10,33 @@ from time import *
 
 # Constants
 
+# Extremum
 Xmax,Ymax=100,100
 
+#Point Class : Classic mathematical modelisation of a point in 2D space 
 
-class P(object):
-	dim=0
+class Point(object):
+	#Initial X,Y
         X,Y=0,0
+	#Creating a Point class dosent need arguments . Every Point get X=0 Y=0 at the creation
 	def __init__(self):
         	self.X=0
         	self.Y=0
+	#Setting values
     	def setXY(self,x,y):
         	self.X=x
         	self.Y=y
-    	def tupl(self,a):
-        	self.X=P[0]
-        	self.Y=P[1]
     	def AVX(self,value):
         	self.X+=value
+	def tupl(self,a):
+        	self.X=P[0]
+        	self.Y=P[1]
     	def AVY(self,value):
         	self.Y+=value
     	def clonePoint(self,Point):
         	self.X=Point.X
         	self.Y=Point.Y
+	#Returning Values
     	def returnXY(self):
         	return self.X,self.Y
     	def X(self):
@@ -39,6 +44,7 @@ class P(object):
     	def Y(self):
         	return self.Y
         
+#The abstract modelisation of an infinit number of points between 2 non-same points
 class Line():
 	A,B=P(),P()
 	def __init__(self,a,b):
@@ -53,13 +59,24 @@ class Line():
 	def setB(self,b):
 		self.B=b
 
-
-def MoveX():
-    	print("x moving")
 	
-LEN=lambda A,B : sqrt( (A.X-B.X)**2 + (A.Y-B.Y)**2)
+Len=lambda A,B : sqrt( (A.X-B.X)**2 + (A.Y-B.Y)**2)
 def GeometriqueDistance(A,B):
-	return LEN(A,B)
+	return Len(A,B)
+
+def generate_points_table(Xmax,Ymax,step):
+	P=Point()
+	tableL=[]
+	finaltable=[]
+	for i in range(0,Xmax+step,step):
+		for j in range(0,Ymax+step,step):
+			P.setXY(i,j)
+			tableL+=[P]
+		finaltable+=[tableL]
+		tableL=0
+	return finaltable
+
+
 #def MoveY():
 
 #def MoveStepX():
