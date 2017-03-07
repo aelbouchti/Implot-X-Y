0a201fa2ed856b480fd8e8f2988b617a692280ef
Hilaly <hilalyamine@gmail.com>
2017-03-07 15:25:40 +0100

    Update Point.py

--- ImplotController/Impcontroler/Point.py
+++ ImplotController/Impcontroler/Point.py
@@ -1,61 +1,66 @@
 
-
-
 # Imperial Creativity
 # ImplotControler 1.0
-# M-A Hilaly
 
 
 # Point class and functions
 
-
-
 from math import *
 from time import *
 
-
-
 # Constants
 
+Xmax,Ymax=100,100
 
-#MotorX,MotorY
-#StepX,StepY
-Xmax,Ymax=20,40
-
-#Grid
-#Data
 
 
 class P(object):
-    dim=0
-    
-    def __init__(self,X,Y):
-        self.X=X
-        self.Y=Y
-        
-    def setXY(self,a,b):
-        self.X=a
-        self.Y=b
-    
-    def setX(self,value):
-        self.X=self.X+value
-        
-    def setY(self,value):
+        dim=0
+        X,Y=0,0
+    def __init__(self):
+        self.X=0
+        self.Y=0
+    def setXY(self,x,y):
+        self.X=x
+        self.Y=y
+    def tuple(self,a):
+        self.X=P[0]
+        self.Y=P[1]
+    def AVX(self,value):
+        self.X+=value
+    def AVY(self,value):
         self.Y+=value
-        
-    def setPoint(self,Point):
+    def clonePoint(self,Point):
         self.X=Point.X
         self.Y=Point.Y
-        
     def returnXY(self):
         return self.X,self.Y
+    def X(self):
+        return self.X
+    def Y(self):
+        return self.Y
         
 
 class Line():
-    pass
+	A,B=P(),P()
+	def __init__(self,a,b):
+		self.A=a
+		self.B=b
+	def GeoLen(self):
+		return sqrt( (self.A.X-self.B.X)**2 + (self.A.Y-self.B.Y)**2)
+	def PixLen(self):
+		return abs(self.A.X-self.B.X) + abs(self.A.Y-self.B.Y)
+	def setA(self,a):
+		self.A=a
+	def setB(self,b):
+		self.B=b
+    
 # Functions
 
 
+    
+    
+    
     
 def MoveX():
     print("x moving")
