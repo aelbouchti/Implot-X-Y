8b725dbee951d0b2a0124abb75a084e8e31646e1
Hilaly <hilalyamine@gmail.com>
2017-03-10 21:00:03 +0100

    Update Point.py

--- ImplotController/Impcontroler/Point.py
+++ ImplotController/Impcontroler/Point.py
@@ -16,7 +16,7 @@ from time import *
 Xmax,Ymax=100,100
 
 
-#Point Class : Classic mathematical modelisation of a point in 2D space 
+#Point Class : Classic mathematical modelisation of a point in 2D space d
 class Point(object):
 	#Initial X,Y
 	def __init__(self,a=0,b=0):
