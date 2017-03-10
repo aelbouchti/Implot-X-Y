4af69f13eada84cb61a409885a44f558993433ab
Hilaly <hilalyamine@gmail.com>
2017-03-10 18:06:16 +0100

    Update Point.py

--- ImplotController/Impcontroler/Point.py
+++ ImplotController/Impcontroler/Point.py
@@ -141,29 +141,23 @@ def bresenham_line(start, end):
 	dx = x2 - x1
 	dy = y2 - y1
     	# Determine how steep the line is
-	
     	is_steep = abs(dy) > abs(dx)
-	
     	# Rotate line
     	if is_steep:
         	x1, y1 = y1, x1
-        	x2, y2 = y2, x2
-		
+        	x2, y2 = y2, x2	
 	# Swap start and end points if necessary and store swap state
         swapped = False
     	if x1 > x2:
         	x1, x2 = x2, x1
         	y1, y2 = y2, y1
         	swapped = True
-
     	# Recalculate differentials
     	dx = x2 - x1
     	dy = y2 - y1
-	
     	# Calculate error
     	error = int(dx / 2.0)
     	ystep = 1 if y1 < y2 else -1
-	
     	# Iterate over bounding box generating points between start and end
     	y = y1
     	points = []
