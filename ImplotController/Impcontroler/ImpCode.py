c0138cdf4bc5fd750ac66c9c5f6b2478291f377f
Hilaly <hilalyamine@gmail.com>
2017-03-15 21:14:03 +0100

    Update ImpCode.py

--- ImplotController/Impcontroler/ImpCode.py
+++ ImplotController/Impcontroler/ImpCode.py
@@ -1,13 +1,40 @@
 
+"""
+General Functions
+Calling Intern Cursor Class
+Generating Paths and Executing Data
+
+"""
+
+
 
 from Impcontroler import Point
 from Impcontroler import Motor
 from time import *
 from math import *
 
-Xmax,Ymax
+M1pins=[]
+M2pins=[]
+S1P,S2P=0,0
+Xmax,Ymax=0,0
 
 
-s,m,n=generate_points_table(Xmax,Ymax,Motorstep)
+def StartX():
+    CURSOR=CURSOR()
+    CURSOR.setPins(M1pins,M2pins)
+    CURSOR.sensorPins(S1P,S2P)
+    CURSOR.HomeCursor()
 
 
+
+def Data(CODE):
+    CURSOR.GetCodeData(CODE)
+
+    
+def WriteData():
+    CURSOR.GeneratePaths()
+    CURSOR.ExecuteData()
+    
+if __name__=="__main__":
+    StartX()
+    WriteData()
