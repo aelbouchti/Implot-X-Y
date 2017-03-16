4e9ae166efd60ded504e3f3a4640bcd73d721aa7
Hilaly <hilalyamine@gmail.com>
2017-03-16 23:16:01 +0100

    Update ImpCode.py

--- ImplotController/Impcontroler/ImpCode.py
+++ ImplotController/Impcontroler/ImpCode.py
@@ -7,13 +7,15 @@ Generating Paths and Executing Data
 """
 
 
-#from Impcontroler import Point
+from Impcontroler import Point
 from Impcontroler import Motor
 #from time import sleep
 #from math import sleep
 
 M1pins = []
 M2pins = []
+Code='0.0.T.1.1.F'
+TargetPoint=Point.Point()
 S1P, S2P = 0, 0
 Xmax, Ymax = 0, 0
 
@@ -23,7 +25,7 @@ def StartX():
     Motor.Motor.sensorPins(S1P, S2P)
     Motor.Motor.HomeCursor()
 
-def Data(CODE):
+def Data(CODE=code):
     Motor.Motor.GetCodeData(CODE)
 
 def WriteData():
@@ -32,4 +34,5 @@ def WriteData():
 
 if __name__ == "__main__":
     StartX()
+    Data(code)
     WriteData()
